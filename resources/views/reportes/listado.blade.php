0e9a74d50bb5c91271922c41fbd6ebebd4d68b83
Juan López <jukamilo2010@hotmail.com>
2023-05-29 16:09:09 -0500

    clase del 29/05/23 configuración de migraciones

---
+++ resources/views/reportes/listado.blade.php
@@ -0,0 +1,24 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Reportes</h1>
+
+
+@stop
+
+@section('content')
+    <p>Listado de Reportes</p>
+
+
+
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Facultades;
+use App\Http\Controllers\Programas;
+use App\Http\Controllers\Profesores;
+use App\Http\Controllers\Estudiantes;
+use App\Http\Controllers\Calificaciones;
+use App\Http\Controllers\Reportes;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', [HomeController::class, 'index']);
+Route::get('/facultades/listado', [Facultades::class, 'index']);
+Route::get('/programas/listado', [Programas::class, 'index']);
+Route::get('/profesores/listado', [Profesores::class, 'index']);
+Route::get('/estudiantes/listado', [Estudiantes::class, 'index']);
+Route::get('/regnotas/listado', [Calificaciones::class, 'index']);
+Route::get('/reportes/listado', [Reportes::class, 'index']);
+
+
+
+
+
+
